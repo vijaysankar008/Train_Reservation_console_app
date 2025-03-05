54448226a1b5132eae41da0189fc8ab64ad27b5c
Vijayakumar <vijayakumar>
2025-03-05 18:01:20 +0530

    Second Commit

---
+++ src/irctc/app2/MainController.java
@@ -0,0 +1,8 @@
+package irctc.app2;
+
+public class MainController {
+	public static void main(String[]a) {
+		
+		System.out.println("******Welcome to ticket booking app********\n1.Booking\n2.Cancel\n3.Print Booked Ticket\n4.Print Available Ticket");
+	} 
+}
